c9fa9d7292973662ce576dcbd5f17e32cb65d90f
Abdulrehman khan <Abdulrehmankhan1010589@gmail.com>
2023-10-26 15:19:53 +0500

    Livewire Counter

---
+++ resources/views/livewire/counter.blade.php
@@ -0,0 +1,6 @@
+
+<div style="text-align: center; background-color:aqua;">
+    <button style="background-color:red;" wire:click="increment">+</button>
+    <h1>{{ $count }}</h1>
+    <button style="background-color:blue;" wire:click="decrement">-</button>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Livewire\Counter;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,9 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('', Counter::class);
+    
+
 
 Route::middleware([
     'auth:sanctum',
